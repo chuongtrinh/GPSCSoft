12fdc2916a0427fb9bef6cf3cc14429dfe1c96a3
roguelider <roguelider@live.com>
2016-04-06 21:35:03 +0000

    Modifying view to match new model

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,16 +1,16 @@
 class Representative < ActiveRecord::Base
     belongs_to :department
     
-     def self.uploadFile(file)
+    def self.uploadFile(file)
          model = Representative
           spreadsheet = SheetsController.open_spreadsheet(file)
           header = spreadsheet.row(1)
           
           (2..spreadsheet.last_row).each do |i|
             row = Hash[[header, spreadsheet.row(i)].transpose]
-            department = find_by_id(row["id"]) || new
-            department.attributes = row.to_hash.slice(*model.attribute_names())
-            department.save!
+            instance = find_by_id(row["id"]) || new
+            instance.attributes = row.to_hash.slice(*model.attribute_names())
+            instance.save!
           end
     end
 
