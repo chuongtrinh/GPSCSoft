2a6b99f52e1e47190095f8d03f810bfe78abdb84
fyzokaytamu <fyzokay@tamu.edu>
2016-04-12 20:06:28 +0000

    fixed a bug

--- app/controllers/representative_controller.rb
+++ app/controllers/representative_controller.rb
@@ -1,7 +1,7 @@
 class RepresentativeController < ApplicationController
     
     def self.update_representative row
-        instance = Representative.find_by_first_name(row["first_name"]) && Representative.find_by_last_name(row["last_name"]) || Representative.new
+        instance = Representative.find_by(first_name: row["first_name"],last_name: row["last_name"])|| Representative.new
         instance.attributes = row.to_hash.slice(*Representative.attribute_names())
         instance.save!
     end
