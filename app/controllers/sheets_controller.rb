1fe7cbaa553c918326a22fb48c10eb8f4f803970
fyzokaytamu <fyzokay@tamu.edu>
2016-04-15 20:41:43 +0000

    read a csv file

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -47,6 +47,7 @@ class SheetsController < ApplicationController
       case File.extname(file.original_filename)
          when ".xls" then Roo::Excel.new(file.path)
             when ".xlsx" then Roo::Excelx.new(file.path)
+               when ".csv" then Eoo::Csv.new(file.path)
             else raise "Unknown file type: #{file.original_filename}"
       end
    end
